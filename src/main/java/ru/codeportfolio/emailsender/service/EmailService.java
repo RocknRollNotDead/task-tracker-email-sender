2d44bb1f2c6f405d3a05f60d47f267ae0ed53d6e
artemka2003 <86artem2003@mail.ru>
2026-08-05 13:09:48 +0300

    add spring mail

--- src/main/java/ru/codeportfolio/emailsender/service/EmailService.java
+++ src/main/java/ru/codeportfolio/emailsender/service/EmailService.java
@@ -1,22 +1,31 @@
 package ru.codeportfolio.emailsender.service;
 
-
-
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import ru.codeportfolio.emailsender.dto.EmailDto;
 
 @Service
 public class EmailService {
 
+    private final JavaMailSender mailSender;
+
 
     // слушает backend и слушает планировщик
 
-    public EmailService() {
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
     }
 
     public void send(EmailDto emailDto){
 
-        // email отправка todo
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(emailDto.email());
+        message.setSubject(emailDto.header());
+        message.setText(emailDto.text());
+
+        mailSender.send(message);
     }
 
 
